71b11bde1e77645ec043a09b5da18ba5555777dc
Alexey <lexanav1245@mail.ru>
2026-09-21 22:16:47 +0300

    add data to database

--- internal/app/ds/RenderServerUnit.go
+++ internal/app/ds/RenderServerUnit.go
@@ -1,13 +1,12 @@
 package ds
 
 type RenderServerUnit struct {
-  	ID int
-	Status string
-	Processor string
-	Cores int
-	RAM int
-	Description string
-	Likes []int
-	ImagePath string
-	VideoPath string
+  	ID			int		`gorm:"primaryKey"`
+	Status		string	`gorm:"type:varchar(10)"`
+	Processor 	string	`gorm:"type:varchar(30)"`
+	Cores 		int
+	RAM 		int
+	Description string	`gorm:"type:varchar(300)"`
+	Image	 	string	`gorm:"type:varchar(100)"`
+	Video	 	string	`gorm:"type:varchar(100)"`
 }
